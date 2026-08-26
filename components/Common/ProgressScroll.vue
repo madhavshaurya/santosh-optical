02b60788bdb6fee5a68864cd7fc5e7767e3a648f
madhavshaurya <115560501+madhavshaurya@users.noreply.github.com>
2026-08-26 09:34:19 +0000

    a11y: add ARIA label and keyboard navigation to scroll to top button

--- components/Common/ProgressScroll.vue
+++ components/Common/ProgressScroll.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="progress-wrap cursor-pointer">
-    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+  <div
+    class="progress-wrap cursor-pointer"
+    role="button"
+    tabindex="0"
+    aria-label="Back to top"
+    @keydown.enter.prevent="handleScrollToTop"
+    @keydown.space.prevent="handleScrollToTop"
+  >
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102" aria-hidden="true">
       <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
     </svg>
   </div>
@@ -14,4 +21,8 @@ import scrollToTop from '@/common/scrollToTop';
 onMounted(() => {
   scrollToTop();
 });
+
+const handleScrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
 </script>
